8389e6494a432c4515952fc25ad6987f54435b51
qq331161120 <331161120@qq.com>
2017-09-20 23:57:56 +0800

    简单Maven实例

---
+++ hello-word-m2e/src/main/java/com/cj/mvnbook/hellowordm2e/App.java
@@ -0,0 +1,13 @@
+package com.cj.mvnbook.hellowordm2e;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
